3150fc60ae258639ea83b092d720b6855b1e93c5
adityawira15 <aditya_wira98@yahoo.com>
2016-09-13 15:38:26 +0700

    matematika dasar

---
+++ math-basics.js
@@ -0,0 +1,38 @@
+// matematic dasar
+
+//pangkat
+
+var a = 2;
+var b = 3;
+var pangkat = function (){
+  alert(Math.pow(a,b));
+}
+pangkat();
+
+// akar kuadrat
+
+var c = number;
+var akarKuadrat = function (number) {
+alert(Math.sqrt(c));
+}
+akarKuadrat(9);
+
+//hipotenusa
+
+var d = 3;
+var e = 4;
+var f = 2;
+var hipotenusa = function() {
+  alert(Math.sqrt((Math.pow(d, f) + (Math.pow(e, f)))))
+}
+hipotenusa();
+
+// keliling lingkaran
+
+var LK = function(number)
+{
+var jari = number;
+var hasil = 3.14*jari*jari;
+alert(hasil);
+};
+LK(7);
