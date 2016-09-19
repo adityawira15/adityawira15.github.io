ab6b319e7870451fbae8844fef91470e575c8b1b
adityawira15 <aditya_wira98@yahoo.com>
2016-09-19 11:39:14 +0700

    xlear

--- contact-list.js
+++ contact-list.js
@@ -1,19 +1,19 @@
-var DaftarKontak = new Array();
-DaftarKontak.Program = new Object();
-DaftarKontak.Program.tambahKontak = function(nama, email, telepon){
+var DaftarKontak = [];
+var Program = new Object();
+Program.tambahKontak = function(nama, email, telepon){
 DaftarKontak[DaftarKontak.length] = {
     nama: nama,
     email: email,
     telepon: telepon,
    }
 };
-DaftarKontak.Program.LihatSemua = function LihatSemua() {
+Program.LihatSemua = function LihatSemua() {
 	var kontak = DaftarKontak.length;
 	for (var i = 0; i < kontak; i++) {
 		console.log(DaftarKontak[i]);
 	}
 };
-DaftarKontak.Program.cariKontak = function cariKontak(nama){
+Program.cariKontak = function cariKontak(nama){
 var kontak = DaftarKontak.length;
     for (var i = 0; i < kontak; i++) {
         if(nama == DaftarKontak[i].nama){
@@ -22,7 +22,7 @@ var kontak = DaftarKontak.length;
     }
 };
 
-DaftarKontak.Program.tambahKontak("Ruben","ruben@gmail.com","021777555");
-DaftarKontak.Program.tambahKontak("gugel","gugel@gmail.com","021777888")
-DaftarKontak.Program.LihatSemua();
-DaftarKontak.Program.cariKontak("Ruben");
+Program.tambahKontak("Ruben","ruben@gmail.com","021777555");
+Program.tambahKontak("gugel","gugel@gmail.com","021777888")
+Program.LihatSemua();
+Program.cariKontak("Ruben");
