a3ebd58da68ce5524be75f7282663034773d45da
adityawira15 <aditya_wira98@yahoo.com>
2016-09-19 12:15:26 +0700

    clear

--- contact-list.js
+++ contact-list.js
@@ -1,27 +1,27 @@
 var DaftarKontak = [];
-var Program = new Object();
-Program.tambahKontak = function(nama, email, telepon){
+var Program = {
+tambahKontak : function(nama, email, telepon){
 DaftarKontak[DaftarKontak.length] = {
     nama: nama,
     email: email,
     telepon: telepon,
    }
-};
-Program.LihatSemua = function LihatSemua() {
+},
+LihatSemua : function LihatSemua() {
 	var kontak = DaftarKontak.length;
 	for (var i = 0; i < kontak; i++) {
 		console.log(DaftarKontak[i]);
 	}
-};
-Program.cariKontak = function cariKontak(nama){
+},
+cariKontak : function cariKontak(nama){
 var kontak = DaftarKontak.length;
     for (var i = 0; i < kontak; i++) {
         if(nama == DaftarKontak[i].nama){
         console.log(DaftarKontak[i]);
         }
     }
-};
-
+}
+}
 Program.tambahKontak("Ruben","ruben@gmail.com","021777555");
 Program.tambahKontak("gugel","gugel@gmail.com","021777888")
 Program.LihatSemua();
